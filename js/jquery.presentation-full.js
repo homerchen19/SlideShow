8e1fc1c6c313ba8dadd83b76d0c4aa6c7b290e53
Homer Chen <xxhomey19@gmail.com>
2015-11-16 20:02:14 +0800

    complete timestamp

--- js/jquery.presentation-full.js
+++ js/jquery.presentation-full.js
@@ -44,14 +44,36 @@
           var action = '';
           if(e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 33) {
             action = 'prev';
-          } else if(e.keyCode === 39 || e.keyCode === 40 || e.keyCode === 34) {
+          } 
+          else if(e.keyCode === 39 || e.keyCode === 40 || e.keyCode === 34) {
             action = 'next';
+            alert($presentation.getFormattedDate());
           }
           
           if(action !== '') {
             $presentation.prevNextClick(action);
           }
         });
+      };
+
+      $presentation.getFormattedDate = function() {
+        var date = new Date();
+
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var hour = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+
+        month = (month < 10 ? "0" : "") + month;
+        day = (day < 10 ? "0" : "") + day;
+        hour = (hour < 10 ? "0" : "") + hour;
+        min = (min < 10 ? "0" : "") + min;
+        sec = (sec < 10 ? "0" : "") + sec;
+
+        var str = date.getFullYear() + "/" + month + "/" + day + " " +  hour + ":" + min + ":" + sec;
+
+        return str;
       };
       
       //Start this thing
@@ -75,3 +97,4 @@
     });
   };
 })(jQuery);
+
